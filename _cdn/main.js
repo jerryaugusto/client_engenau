b8d9d03d1efc5a1b46e8db9c83127738e022bf19
jerryaugusto <jerryaugusto13@gmail.com>
2020-06-16 22:18:15 -0400

    Revised Project: 'btn_back' Hidden by default / Redesign.

--- _cdn/main.js
+++ _cdn/main.js
@@ -1,28 +1,31 @@
 // MENU MOBILE
-$(function() {
-  $('.main_header_content_menu_mobile_obj').on('click', function() {
-    $('.main_header_content_menu_mobile_sub').toggleClass('ds_none');
-    $(this).toggleClass('main_header_content_menu_mobile_obj_active');
-  });
+$(function () {
+    $('.main_header_content_menu_mobile_obj').on('click', function () {
+        $('.main_header_content_menu_mobile_sub').toggleClass('ds_none');
+        $(this).toggleClass('main_header_content_menu_mobile_obj_active');
+    });
 });
 
 // SMOTH SCROLL
 var $doc = $('html, body');
-$('.smothScroll').click(function(){
-  $doc.animate({
-    scrollTop:$($.attr(this,'href')).offset().top
-  }, 700);
-  return false;
+$('.smothScroll').click(function () {
+    $doc.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+    return false;
 });
 
 // LOCAL VARIABLES
-var _containerBtn = $(".btn_back");
+let _containerBtn = $(".btn_back");
+$window = 0;
 
 // BACK TO TOP ACTION
-$(window).scroll(function(){
-  if($(window).scrollTop() > 400){
-    _containerBtn.fadeIn();
-  }else{
-    _containerBtn.fadeOut();
-  }
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 400) {
+        _containerBtn.fadeIn();
+    } else {
+        _containerBtn.fadeOut();
+    }
 });
+
+
